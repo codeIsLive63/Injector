c2e6500747be78d2d36d41c49a640ab3caf0c3f8
Kalmykov-Nikita263 <nikita.kalmykov263@gmail.com>
2023-07-22 23:05:38 +0400

    Пример использования

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,41 @@
 import collections.generic.List;
+import jenumerable.JEnumerable;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new List<>();
+        List<Person> personList = new List<>(new Person[]{
+                new Person("Tom", 19),
+                new Person("Nikita", 18),
+                new Person("Andrey", 20),
+                new Person("Seele", 24),
+                new Person("Gepard", 25),
+                new Person("Arlan", 21),
+                new Person("Alex", 34)
+        });
 
-        list.add(1);
-        list.add(2);
-        list.add(3);
-
-        list.forEach(System.out::println);
-
-        for (Integer integer : list) {
-            System.out.println(integer);
+        for (String personName : JEnumerable.from(personList)
+                .where(p -> p.getName().startsWith("A"))
+                .select(Person::getName)) {
+            System.out.println(personName);
         }
+    }
+}
+
+class Person {
+    private final String _name;
+
+    private final int _age;
+
+    public Person(String name, int age) {
+        _name = name;
+        _age = age;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public int getAge() {
+        return _age;
     }
 }
