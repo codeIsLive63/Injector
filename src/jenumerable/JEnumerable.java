4893d7131a0dea935912cc55734841da2dcc9424
codeislive <codeislive63@gmail.com>
2023-07-29 21:25:32 +0400

    Добавлен метод selectMany, который дополнительно включает 3 перегрузки

--- src/jenumerable/JEnumerable.java
+++ src/jenumerable/JEnumerable.java
@@ -82,6 +82,66 @@ public class JEnumerable<TSource> implements IEnumerable<TSource> {
 	}
 
 	/**
+	 * Производит проекцию каждого элемента списка на другой список элементов, с использованием
+	 * указанного селектора и объединяет результаты в один список.
+	 *
+	 * @param selector Функция-селектор, которая принимает элемент типа TSource и возвращает
+	 *                 перечисление элементов типа TResult.
+	 * @param <TResult> Тип элементов результирующего списка.
+	 * @return Новый экземпляр JEnumerable, содержащий элементы из проекций с использованием селектора.
+	 */
+	public <TResult> JEnumerable<TResult> selectMany(Func<TSource, IEnumerable<TResult>> selector) {
+		return new JEnumerable<>(new SelectManyIterator<>(_enumerator, selector));
+	}
+
+	/**
+	 * Производит проекцию каждого элемента списка на другой список элементов, с использованием
+	 * указанного селектора, передавая также индекс элемента, и объединяет результаты в один список.
+	 *
+	 * @param selector Функция-селектор, которая принимает элемент типа TSource и его индекс,
+	 *                 и возвращает перечисление элементов типа TResult.
+	 * @param <TResult> Тип элементов результирующего списка.
+	 * @return Новый экземпляр JEnumerable, содержащий элементы из проекций с использованием селектора и индекса.
+	 */
+	public <TResult> JEnumerable<TResult> selectMany(BiFunc<TSource, Integer, IEnumerable<TResult>> selector) {
+		return new JEnumerable<>(new SelectManyIndexedIterator<>(_enumerator, selector));
+	}
+
+	/**
+	 * Производит проекцию каждого элемента списка на другой список элементов с использованием
+	 * указанного селектора, а затем объединяет результаты в один список с использованием
+	 * указанного селектора результата.
+	 *
+	 * @param collectionSelector Функция-селектор, которая принимает элемент типа TSource и возвращает
+	 *                           перечисление элементов типа TCollection.
+	 * @param resultSelector     Функция-селектор результата, которая принимает элемент типа TSource
+	 *                           и элемент типа TCollection, и возвращает элемент типа TResult.
+	 * @param <TCollection>      Тип элементов промежуточного списка.
+	 * @param <TResult>          Тип элементов результирующего списка.
+	 * @return Новый экземпляр JEnumerable, содержащий элементы из проекций с использованием селекторов.
+	 */
+	public <TCollection, TResult> JEnumerable<TResult> selectMany(Func<TSource, IEnumerable<TCollection>> collectionSelector, BiFunc<TSource, TCollection, TResult> resultSelector) {
+		return new JEnumerable<>(new SelectManyResultIterator<>(_enumerator, collectionSelector, resultSelector));
+	}
+
+	/**
+	 * Производит проекцию каждого элемента списка на другой список элементов, с использованием
+	 * указанного селектора и индекса, а затем объединяет результаты в один список с использованием
+	 * указанного селектора результата.
+	 *
+	 * @param collectionSelector Функция-селектор, которая принимает элемент типа TSource и его индекс,
+	 *                           и возвращает перечисление элементов типа TCollection.
+	 * @param resultSelector     Функция-селектор результата, которая принимает элемент типа TSource,
+	 *                           элемент типа TCollection и индекс элемента, и возвращает элемент типа TResult.
+	 * @param <TCollection>      Тип элементов промежуточного списка.
+	 * @param <TResult>          Тип элементов результирующего списка.
+	 * @return Новый экземпляр JEnumerable, содержащий элементы из проекций с использованием селекторов и индекса.
+	 */
+	public <TCollection, TResult> JEnumerable<TResult> selectMany(BiFunc<TSource, Integer, IEnumerable<TCollection>> collectionSelector, BiFunc<TSource, TCollection, TResult> resultSelector) {
+		return new JEnumerable<>(new SelectManyIndexedResultIterator<>(_enumerator, collectionSelector, resultSelector));
+	}
+
+	/**
 	 * Фильтрует элементы коллекции на основе указанного лямбда-выражения.
 	 * <p>
 	 * @param predicate Лямбда-выражение для проверки каждого элемента на наличие условия.
@@ -165,6 +225,7 @@ public class JEnumerable<TSource> implements IEnumerable<TSource> {
 			_enumerator = enumerator;
 			_selector = selector;
 			_indexedSelector = null;
+			_index = -1;
 		}
 
 		/**
@@ -178,6 +239,7 @@ public class JEnumerable<TSource> implements IEnumerable<TSource> {
 			_enumerator = enumerator;
 			_selector = null;
 			_indexedSelector = selector;
+			_index = -1;
 		}
 
 		@Override
@@ -191,10 +253,8 @@ public class JEnumerable<TSource> implements IEnumerable<TSource> {
 						_current = _selector.apply(_enumerator.getCurrent());
 					}
 				}
-
 				return true;
 			}
-
 			return false;
 		}
 
@@ -249,8 +309,8 @@ public class JEnumerable<TSource> implements IEnumerable<TSource> {
 		private WhereIterator(IEnumerator<TSource> enumerator, BiFunc<TSource, Integer, Boolean> predicate) {
 			_enumerator = enumerator;
 			_predicate = null;
-			_index = -1;
 			_indexedPredicate = predicate;
+			_index = -1;
 		}
 
 		@Override
@@ -260,12 +320,10 @@ public class JEnumerable<TSource> implements IEnumerable<TSource> {
 				_current = _enumerator.getCurrent();
 				if (_predicate != null && _predicate.test(_current)) {
 					return true;
-				}
-				if (_indexedPredicate != null && _indexedPredicate.apply(_current, _index)) {
+				} else if (_indexedPredicate != null && _indexedPredicate.apply(_current, _index)) {
 					return true;
 				}
 			}
-
 			return false;
 		}
 
@@ -279,6 +337,234 @@ public class JEnumerable<TSource> implements IEnumerable<TSource> {
 			_enumerator.reset();
 			_index = -1;
 			_current = null;
+		}
+	}
+
+	/**
+	 * Внутренний класс, реализующий интерфейс IEnumerator для выполнения операции
+	 * selectMany над элементами списка типа TSource.
+	 *
+	 * @param <TSource> Тип элементов исходного списка.
+	 * @param <TResult> Тип элементов результирующего списка.
+	 */
+	private static class SelectManyIterator<TSource, TResult> implements IEnumerator<TResult> {
+
+		private final IEnumerator<TSource> _enumerator;
+		private final Func<TSource, IEnumerable<TResult>> _selector;
+		private IEnumerator<TResult> _currentEnumerator;
+
+		/**
+		 * Инициализирует новый экземпляр SelectManyIterator.
+		 *
+		 * @param enumerator Итератор исходного списка типа TSource.
+		 * @param selector   Функция-селектор, которая принимает элемент типа TSource
+		 *                   и возвращает перечисление элементов типа TResult.
+		 */
+		private SelectManyIterator(IEnumerator<TSource> enumerator, Func<TSource, IEnumerable<TResult>> selector) {
+			_enumerator = enumerator;
+			_selector = selector;
+			_currentEnumerator = null;
+		}
+
+		@Override
+		public boolean moveNext() {
+			while (_currentEnumerator == null || !_currentEnumerator.moveNext()) {
+				if (_enumerator.moveNext()) {
+					_currentEnumerator = _selector.apply(_enumerator.getCurrent()).getEnumerator();
+				} else {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		@Override
+		public TResult getCurrent() {
+			return _currentEnumerator.getCurrent();
+		}
+
+		@Override
+		public void reset() {
+			_enumerator.reset();
+			_currentEnumerator = null;
+		}
+	}
+
+	/**
+	 * Внутренний класс, реализующий интерфейс IEnumerator для выполнения операции
+	 * selectMany над элементами списка типа TSource, с использованием индекса элементов
+	 *
+	 * @param <TSource>  Тип элементов исходного списка.
+	 * @param <TResult>  Тип элементов результирующего списка.
+	 */
+	private static class SelectManyIndexedIterator<TSource, TResult> implements IEnumerator<TResult> {
+
+		private final IEnumerator<TSource> _enumerator;
+		private final BiFunc<TSource, Integer, IEnumerable<TResult>> _selector;
+		private IEnumerator<TResult> _currentEnumerator;
+		private int _index;
+
+		/**
+		 * Инициализирует новый экземпляр класса SelectManyIndexedIterator.
+		 *
+		 * @param enumerator Итератор элементов исходного списка.
+		 * @param selector   Функция-селектор, которая принимает элемент типа TSource и его индекс,
+		 *                   и возвращает перечисление элементов типа TResult.
+		 */
+		private SelectManyIndexedIterator(IEnumerator<TSource> enumerator, BiFunc<TSource, Integer, IEnumerable<TResult>> selector) {
+			_enumerator = enumerator;
+			_selector = selector;
+			_currentEnumerator = null;
+			_index = -1;
+		}
+
+		@Override
+		public boolean moveNext() {
+			while (_currentEnumerator == null || !_currentEnumerator.moveNext()) {
+				if (_enumerator.moveNext()) {
+					_index++;
+					_currentEnumerator = _selector.apply(_enumerator.getCurrent(), _index).getEnumerator();
+				} else {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		@Override
+		public TResult getCurrent() {
+			return _currentEnumerator.getCurrent();
+		}
+
+		@Override
+		public void reset() {
+			_enumerator.reset();
+			_currentEnumerator = null;
+			_index = -1;
+		}
+	}
+
+	/**
+	 * Внутренний класс, реализующий интерфейс IEnumerator, который выполняет проекцию каждого элемента списка
+	 * на другой список элементов с использованием указанного селектора, а затем объединяет результаты
+	 * в один список с использованием указанного селектора результата.
+	 *
+	 * @param <TSource>       Тип элементов исходного списка.
+	 * @param <TCollection>   Тип элементов промежуточного списка, полученного с использованием селектора.
+	 * @param <TResult>       Тип элементов результирующего списка, полученного с использованием селектора результата.
+	 */
+	private static class SelectManyResultIterator<TSource, TCollection, TResult> implements IEnumerator<TResult> {
+
+		private final IEnumerator<TSource> _enumerator;
+		private final Func<TSource, IEnumerable<TCollection>> _collectionSelector;
+		private final BiFunc<TSource, TCollection, TResult> _resultSelector;
+		private IEnumerator<TSource> _currentEnumerator;
+		private IEnumerator<TCollection> _currentCollectionEnumerator;
+
+		/**
+		 * Инициализирует новый экземпляр класса SelectManyResultIterator.
+		 *
+		 * @param enumerator         Итератор исходного списка элементов типа TSource.
+		 * @param collectionSelector Функция-селектор, которая принимает элемент типа TSource
+		 *                           и возвращает перечисление элементов типа TCollection.
+		 * @param resultSelector     Функция-селектор результата, которая принимает элемент типа TSource,
+		 *                           элемент типа TCollection и возвращает элемент типа TResult.
+		 */
+		private SelectManyResultIterator(IEnumerator<TSource> enumerator, Func<TSource, IEnumerable<TCollection>> collectionSelector, BiFunc<TSource, TCollection, TResult> resultSelector) {
+			_enumerator = enumerator;
+			_collectionSelector = collectionSelector;
+			_resultSelector = resultSelector;
+			_currentEnumerator = null;
+			_currentCollectionEnumerator = null;
+		}
+
+		@Override
+		public boolean moveNext() {
+			while (_currentEnumerator == null || !_currentCollectionEnumerator.moveNext()) {
+				if (_enumerator.moveNext()) {
+					_currentEnumerator = _enumerator;
+					_currentCollectionEnumerator = _collectionSelector.apply(_enumerator.getCurrent()).getEnumerator();
+				} else {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		@Override
+		public TResult getCurrent() {
+			return _resultSelector.apply(_currentEnumerator.getCurrent(), _currentCollectionEnumerator.getCurrent());
+		}
+
+		@Override
+		public void reset() {
+			_enumerator.reset();
+			_currentEnumerator = null;
+			_currentCollectionEnumerator = null;
+		}
+	}
+
+	/**
+	 * Внутренний класс, который реализует интерфейс IEnumerator для проекции каждого элемента списка
+	 * на другой список элементов с использованием указанного селектора и индекса, а затем объединения
+	 * результатов в один список с использованием указанного селектора результата.
+	 *
+	 * @param <TSource>         Тип исходных элементов списка.
+	 * @param <TCollection>     Тип элементов промежуточного списка.
+	 * @param <TResult>         Тип элементов результирующего списка.
+	 */
+	private static class SelectManyIndexedResultIterator<TSource, TCollection, TResult> implements IEnumerator<TResult> {
+
+		private final IEnumerator<TSource> _enumerator;
+		private final BiFunc<TSource, Integer, IEnumerable<TCollection>> _collectionSelector;
+		private final BiFunc<TSource, TCollection, TResult> _resultSelector;
+		private IEnumerator<TSource> _currentEnumerator;
+		private IEnumerator<TCollection> _currentCollectionEnumerator;
+		private int _index;
+
+		/**
+		 * Инициализирует новый экземпляр класса SelectManyIndexedResultIterator.
+		 *
+		 * @param enumerator         Итератор исходного списка элементов типа TSource.
+		 * @param collectionSelector Функция-селектор, которая принимает элемент типа TSource и его индекс,
+		 *                           и возвращает перечисление элементов типа TCollection.
+		 * @param resultSelector     Функция-селектор результата, которая принимает элемент типа TSource,
+		 *                           элемент типа TCollection и индекс элемента, и возвращает элемент типа TResult.
+		 */
+		private SelectManyIndexedResultIterator(IEnumerator<TSource> enumerator, BiFunc<TSource, Integer, IEnumerable<TCollection>> collectionSelector, BiFunc<TSource, TCollection, TResult> resultSelector) {
+			_enumerator = enumerator;
+			_collectionSelector = collectionSelector;
+			_resultSelector = resultSelector;
+			_currentEnumerator = null;
+			_currentCollectionEnumerator = null;
+			_index = -1;
+		}
+
+		@Override
+		public boolean moveNext() {
+			while (_currentEnumerator == null || !_currentCollectionEnumerator.moveNext()) {
+				if (_enumerator.moveNext()) {
+					_index++;
+					_currentEnumerator = _enumerator;
+					_currentCollectionEnumerator = _collectionSelector.apply(_enumerator.getCurrent(), _index).getEnumerator();
+				} else {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		@Override
+		public TResult getCurrent() {
+			return _resultSelector.apply(_currentEnumerator.getCurrent(), _currentCollectionEnumerator.getCurrent());
+		}
+
+		@Override
+		public void reset() {
+			_enumerator.reset();
+			_currentEnumerator = null;
+			_currentCollectionEnumerator = null;
+			_index = -1;
 		}
 	}
 }
